52130cec2bb2a5a39af1f90e70ae29dd16c626a2
Raul Chacon <chaconrj@morrisville.edu>
2014-06-15 18:42:45 -0400

    Fixes #10 1. Rename module from wp-admin to wpadmin due to errors in the build process. 2. Add license to wpadmin.py 3. Remove starttheme and startplugin from within a class to standalone functions since they were staticmethods anyways. 4. Refactor main 5. Convert README from markdown to rst. 6. create setup.py

---
+++ README.rst
@@ -0,0 +1,80 @@
+===========
+wpadmin
+===========
+
+wpadmin is a Python module for managing WordPress installations from the
+command line. Inspiration comes from both django-admin.py and `WP-CLI`_.
+
+.. _WP-CLI: https://github.com/wp-cli/wp-cli
+
+
+Installing
+---------------------
+
+::
+
+  $ pip install wpadmin
+
+
+Usage
+-------------------
+From the root of your WordPress project (directory containing wp-load.php):
+
+::
+
+  $ wpadmin starttheme [--classic] my-theme-name
+
+
+The ``starttheme`` command will create the following directory structure
+within the wp-contents/themes/ directory:
+
+::
+
+  - my-theme-name
+  - templates/
+    - base.twig
+  - static/
+    - css/
+    - images/
+    - js/
+  - 404.php
+  - functions.php
+  - index.php
+  - README.md
+  - style.css
+
+
+The above structure assumes you will be using the Twig template engine via
+the `Timber  <https://github.com/jarednova/timber>`_
+plugin. The ``starttheme`` command with the ``--classic`` option
+will create the following directory structure within the wp-contents/themes/
+directory:
+
+::
+
+  - my-theme-name
+    - css/
+    - images/
+    - js/
+    - 404.php
+    - functions.php
+    - index.php
+    - README.md
+    - style.css
+
+
+To create a plugin skeleton:
+
+::
+
+  $ wpadmin startplugin my-plugin-name
+
+
+The ``startplugin`` command will create the following directory structure
+within the wp-contents/plugins/ directory:
+
+::
+
+  - my-plugin-name
+    - my-plugin.name.php
+    - README.md
---
+++ wpadmin.py
@@ -0,0 +1,137 @@
+#!/c/Python27/python
+# wpadmin.py - Command line tool for WordPress
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Raul Chacon <raulchacon@outlook.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+r"""
+For usage and a list of options, try this:
+$ python wpadmin.py -h
+"""
+__version__ = '0.1.0'
+
+import os
+from functools import wraps
+
+
+class NotInWordPressRootError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+def wp_root(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if (os.path.isdir(os.path.join(os.getcwd(), 'wp-content',
+            'themes')) and
+            os.path.isdir(os.path.join(os.getcwd(), 'wp-content',
+                          'plugins'))):
+            return f(*args, **kwargs)
+
+        raise NotInWordPressRootError('You must run this script from \
+                                      the WordPress root folder.')
+    return decorated_function
+
+
+@wp_root
+def starttheme(args):
+    """Creates theme folder with the following empty files/folders:
+    index.php, style.css, images/, css/ and js/. If with_timber is
+    True then it additionally creates views/base.twig
+    """
+    # Create Theme folder
+    theme_root = os.path.join(os.getcwd(), 'wp-content', 'themes',
+                              args.name)
+    os.makedirs(theme_root)
+
+    # Create files
+    for f in ['index.php', 'README.md', 'style.css', 'functions.php',
+              '404.php']:
+        fh = open(os.path.join(theme_root, f), 'w')
+        if '.php' in f:
+            fh.write("<?php\n\n")
+        fh.close()
+
+    if args.classic:
+        static_dir = theme_root
+    else:
+        # Create a static sub directory
+        static_dir = os.path.join(theme_root, 'static')
+        os.makedirs(static_dir)
+
+        # Change default twig directory from "views" to "templates"
+        functionsphp = open(os.path.join(theme_root, 'functions.php'), 'a')
+        functionsphp.write("Timber::$dirname = 'templates';\n")
+        functionsphp.close()
+        twig_templates = os.path.join(theme_root, 'templates')
+        os.makedirs(os.path.join(twig_templates))
+        open(os.path.join(twig_templates, 'base.twig'), 'a').close()
+
+    # Create sub directories
+    for d in ['images', 'css', 'js']:
+        os.makedirs(os.path.join(static_dir, d))
+
+
+@wp_root
+def startplugin(args):
+    """Creates plugin folder with a php file of the same name."""
+    plugin_root = os.path.join(os.getcwd(), 'wp-content', 'plugins',
+                               args.name)
+    os.makedirs(plugin_root)
+
+    open(os.path.join(plugin_root, 'README.md'), 'a').close()
+    f = open(os.path.join(plugin_root, args.name + '.php'), 'w')
+    f.write("<?php\n\n")
+    f.close()
+
+
+def _main():
+    """Parse options and execute wpadmin commands"""
+    import argparse
+
+    # Create top level parser
+    parser = argparse.ArgumentParser(description="Create WordPress \
+                                     theme/plugin skeleton")
+    subparsers = parser.add_subparsers()
+
+    # Create the parser for the "starttheme" command
+    parser_starttheme = subparsers.add_parser('starttheme')
+    parser_starttheme.add_argument('name')
+    parser_starttheme.add_argument("-c", "--classic", help="create classic theme \
+                                    skeleton", action="store_true")
+    parser_starttheme.set_defaults(func=starttheme)
+
+    # Create the parser for the "startplugin" command
+    parser_startplugin = subparsers.add_parser('startplugin')
+    parser_startplugin.add_argument('name')
+    parser_startplugin.set_defaults(func=startplugin)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+
+    _main()
